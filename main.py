af7614bb312359e3cb94d9e233199c5cc149d6bf
zsdangdnag <mkcffj@gmail.com>
2024-11-11 02:55:43 +0900

    mars

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('준범이를 위한 스타전적 서버')
 
-pg = st.navigation([st.Page("junbum.py"),st.Page("page_2.py")])
+pg = st.navigation([st.Page("mars.py"),st.Page("zangsu.py")])
 #human = st.selectbox('Pick user', ['준범','민규','환유'])
 
 pg.run()

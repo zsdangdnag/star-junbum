3da9efb3d9fe2babab87a7bcd6512d39fc73fd14
zsdangdnag <mkcffj@gmail.com>
2024-11-11 02:03:11 +0900

    s

---
+++ main.py
@@ -0,0 +1,11 @@
+jun_vic = 0
+jun_fail = 0
+
+import streamlit as st
+
+st.title('준범이를 위한 스타전적 서버')
+
+pg = st.navigation([st.Page("page_1.py"),st.Page("page_2.py")])
+human = st.selectbox('Pick user', ['준범','민규','환유'])
+
+pg.run()
